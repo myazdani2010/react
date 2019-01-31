bd051e79ac6374cef5962928b184bf18b84e2028
Mohammad Yazdani <m.yazdani2010@gmail.com>
2019-01-31 11:10:36 -0800

    introduce state for for App class

--- src/App.js
+++ src/App.js
@@ -3,13 +3,32 @@ import "./App.css";
 import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Max", age: 28 },
+      { name: "Manu", age: 29 },
+      { name: "Stephanie", age: 31 }
+    ]
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Hello from React</h1>
-        <Person name="Max" age="24" />
-        <Person name="Manu" age="32" />
-        <Person name="John" age="22" />
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
+          My Hobbies: Racing
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
       </div>
     );
   }
