93541eec6ea78ff46b7d43cc6fdc9a47970b5fa1
Mohammad Yazdani <m.yazdani2010@gmail.com>
2019-01-31 13:40:05 -0800

    Introduce styling with new css file for Person component

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./Person.css";
 
 const person = props => {
   return (
-    <div>
+    <div className="Person">
       {/* use the click from props to call the method defined in that click */}
       <p onClick={props.click}>
         I'm {props.name}, and I am {props.age} years old!
