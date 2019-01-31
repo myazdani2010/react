bed82959d9d8520f18b7900b975a69c964d42ff6
Mohammad Yazdani <m.yazdani2010@gmail.com>
2019-01-31 09:09:13 -0800

    simple react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Hello from React</h1>
+        <Person name="Max" age="24" />
+        <Person name="Manu" age="32" />
+        <Person name="John" age="22" />
       </div>
     );
   }
