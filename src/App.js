690e68b85362eb644aa617336c689a08afea479d
Mohammad Yazdani <m.yazdani2010@gmail.com>
2019-02-05 13:40:34 -0800

    add persons toggle for conditional dynamic changes

--- src/App.js
+++ src/App.js
@@ -19,7 +19,9 @@ class App extends Component {
         { name: nameParam, age: 28 },
         { name: "Manu", age: 29 },
         { name: "Stephanie", age: 22 }
-      ]
+      ],
+      otherState: "some other value",
+      showPerson: false
     });
   };
 
@@ -31,6 +33,11 @@ class App extends Component {
         { name: "Stephanie", age: 26 }
       ]
     });
+  };
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPerson;
+    this.setState({ showPerson: !doesShow });
   };
 
   render() {
@@ -47,28 +54,29 @@ class App extends Component {
         <h1>Hello from React</h1>
         {/* NOTE: don't add the parantecies for the method in the onClick */}
         {/* NOTE: the arrow function is not ideal way here due to performance insufficiency */}
-        <button
-          style={inlineStyle}
-          onClick={() => this.switchNameHandler("Maximilian!!")}
-        >
-          Swith me
+        <button style={inlineStyle} onClick={this.togglePersonsHandler}>
+          Toggle Persons
         </button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, "Max!!")} // on clicking this Person Paragrapth the method switchNameHandler will be called
-          changed={this.nameChangedHandler}
-        >
-          My Hobbies: Racing
-        </Person>
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-        />
+        {this.state.showPerson ? (
+          <div>
+            <Person
+              name={this.state.persons[0].name}
+              age={this.state.persons[0].age}
+            />
+            <Person
+              name={this.state.persons[1].name}
+              age={this.state.persons[1].age}
+              click={this.switchNameHandler.bind(this, "Max!!")} // on clicking this Person Paragrapth the method switchNameHandler will be called
+              changed={this.nameChangedHandler}
+            >
+              My Hobbies: Racing
+            </Person>
+            <Person
+              name={this.state.persons[2].name}
+              age={this.state.persons[2].age}
+            />
+          </div>
+        ) : null}
       </div>
     );
   }
